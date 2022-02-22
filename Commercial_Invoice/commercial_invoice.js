0884593ea609596f9f081f94ed4d91f83f48df7e
Ben S <ben@email.com>
2022-02-22 17:08:13 -0600

    -

--- Commercial_Invoice/commercial_invoice.js
+++ Commercial_Invoice/commercial_invoice.js
@@ -1,55 +1,44 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableElement/insertRow
 // https://snippet-generator.app/
-// Test comment! :)
 
 let units = [
-    {
-        qty: 1,
-        unitOfMeasure: "Pounds",
-        desc: "Harman & Heatilator Vacuum Pressure Differential Switch, OEM",
-        weight: 0.2,
-        value: 20.95,
-    },
-    {
-        qty: 2,
-        unitOfMeasure: "Pounds",
-        desc: "Misc. Item",
-        weight: 5,
-        value: 30,
-    },
-    {
-        qty: 1,
-        unitOfMeasure: "Pounds",
-        desc: "Harman Igniter - AMP-HIFIN",
-        weight: 0.1,
-        value: 70.99,
-    },
+    [
+        1,
+        "Pounds",
+        "Harman & Heatilator Vacuum Pressure Differential Switch, OEM",
+        0.2,
+        20.95,
+    ],
+    [2, "Pounds", "Misc. Item", 5, 30],
+    [1, "Pounds", "Harman Igniter - AMP-HIFIN", 0.1, 70.99],
 ];
 
 let itemTable = document.getElementById("commodities"); // get table data
 
-function addItems(obj) {
-    // Iterate over an array: for...of
-    for (let entries of obj) {
-        let newRow = itemTable.insertRow(-1); // Insert row at end of table
-        let i = 1;
-        // Iterate over an object: for...in
+function addItems(array) {
+    // Iterate over an array: for... of
+    for (let entries of array) {
+        let newRow = itemTable.insertRow(-1); // Insert new row at end of table
         for (let property in entries) {
-            let newCell = newRow.insertCell(obj[i + 1]);
+            let newCell = newRow.insertCell(array[property + 1]);
             let newText = document.createTextNode(entries[property]);
             newCell.appendChild(newText);
-            i++;
         }
     }
 }
 
+function addWeights() {
+    // iterate over an array. Get all the weights (index[3] of each item.)
+    let weights = [];
+    for (let line of units) {
+        weights.push(line[3]); // push weight column into its own array
+    }
+    // total the weights up
+    return (totalWeight = weights.reduce((total, i) => total + i));
+}
+
 addItems(units);
+addWeights();
 
-// Get all item weights and put them into an array.
-const weights = units.map((itemWeight) => {
-    return itemWeight.weight;
-});
-
-// Total the weights up
-let totalWeight = weights.reduce((total, i) => total + i);
-console.log(totalWeight);
+let shipTotalWeight = document.getElementById("totalweight");
+shipTotalWeight.innerText = totalWeight + " lbs."; // assign total weight;
