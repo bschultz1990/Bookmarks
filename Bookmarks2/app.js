57ff2221381ceaacf782db0b1734e99b880916ec
Ben Schultz <ben@schultz.com>
2022-07-06 16:55:58 -0500

    Added files via CMD

--- Bookmarks2/app.js
+++ Bookmarks2/app.js
@@ -6,5 +6,28 @@ for (const index of temp0) {
 console.dir(linkTitles);
 
 const search = function search() {
-    const searchBox = document.getElementById("#search-item").value.toUpperCase();
+    const searchTerm = document.getElementById("#search-item").value.toUpperCase();
+
 }
+
+// const search = function search() {
+//     const searchbox = document.getElementById("search-item").value.toUpperCase();
+//     const storeitems = document.getElementById("product-list");
+//     const product = document.querySelectorAll(".product");
+//     const name = document.getElementsByTagName("h2");
+
+//     for (const index of name) {
+//         let match = product[index].getElementsByTagName('h2')[0]
+
+//         if (match) {
+//             let textvalue = match.textContent || match.innerHTML
+
+//             if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+//                 product[i].style.display = ""
+//             else {
+//             product[i].style.display = "none"
+//             }
+//         }
+//         }
+//     }
+// }
