4b69e20dc18572953d656d8581912b7684d6a5b6
Ben Schultz <ben@schultz.com>
2022-12-15 21:14:38 -0600

    Added files via CMD

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ const sList = document.createElement("ul")
 sList.id = "sList"
 sPanel.appendChild(sList)
 
-for (let i=0; i <= array.length(list.title)-1; i++){
+for (let i=0; i <= list.title.length-1; i++){
 	const li = document.createElement("li");
+	li.classList.add("nodisplay")
 	const a = document.createElement("a");
 	const aNode = document.createTextNode(list.title[i])
 	// Append text node to anchor element:
@@ -29,24 +30,53 @@ for (let i=0; i <= array.length(list.title)-1; i++){
 	li.appendChild(a);
 }
 
+// function search() {
+// Get the search query and convert it to uppercase:
+// let searchbox = document.getElementById("search-item").value.toUpperCase();
+
+// Get the <ul> element that contains the search results:
+// let sList = document.querySelector('#sList');
+
+// Iterate through the list.title array:
+// for (let i = 0; i < list.title.length; i++) {
+// Get the current title and convert it to uppercase:
+// let title = list.title[i].toUpperCase();
+
+// Check if the title matches the search query:
+// if (title.includes(searchbox)) {
+// If a match is found, remove the "nodisplay" class from the corresponding <li> element:
+// let li = sList.children[i];
+// li.classList.remove("nodisplay");
+// }
+// }
+// }
 function search() {
 	let searchbox = document.getElementById("search-item").value.toUpperCase();
 	let storeitems = document.querySelector('.product-list');
+	let sList = document.querySelector('#sList');
 
 	if (searchbox == "") {
-		console.log("no stuff in box");
 		storeitems.classList.remove("nodisplay");
 		sPanel.classList.add("nodisplay")
 
 	} else {
-		console.log("stuff in search box");
 		storeitems.classList.add("nodisplay");
 		sPanel.classList.remove("nodisplay");
 	}
 
-	// for (const index of name){
-}
+	// Iterate through the list.title array:
+	for (let i = 0; i < list.title.length; i++) {
+		// Get the current title and convert it to uppercase:
+		let title = list.title[i].toUpperCase();
 
+		
+		if (title.includes(searchbox)) {
+			// If a match is found, remove the "nodisplay" class from the corresponding <li> element:
+			let li = sList.children[i];
+			li.classList.toggle("nodisplay");
+		}
+	}
+}
 // TODO: Make an accordionated list of all bookmarks.
 // Add a key press event listener to the document /
 // When that fires, pull down a custom serach box and dim the rest of the screen.
